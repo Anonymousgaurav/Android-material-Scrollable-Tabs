9fa8874adea1653ff252063b2d383b8a24f1f43d
Gaurav <gaurav@mtouchlabs.com>
2021-01-03 14:24:46 +0530

    material scrollable tabs android

---
+++ app/src/main/java/com/gaurav/android_material_tabs/activity/MainActivity.java
@@ -0,0 +1,61 @@
+package com.gaurav.android_material_tabs.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.gaurav.android_material_tabs.R;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Toolbar toolbar;
+    private Button btnSimpleTabs, btnScrollableTabs, btnIconTextTabs, btnIconTabs, btnCustomIconTextTabs;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        btnSimpleTabs = (Button) findViewById(R.id.btnSimpleTabs);
+        btnScrollableTabs = (Button) findViewById(R.id.btnScrollableTabs);
+        btnIconTextTabs = (Button) findViewById(R.id.btnIconTextTabs);
+        btnIconTabs = (Button) findViewById(R.id.btnIconTabs);
+        btnCustomIconTextTabs = (Button) findViewById(R.id.btnCustomIconTabs);
+
+        btnSimpleTabs.setOnClickListener(this);
+        btnScrollableTabs.setOnClickListener(this);
+        btnIconTextTabs.setOnClickListener(this);
+        btnIconTabs.setOnClickListener(this);
+        btnCustomIconTextTabs.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnSimpleTabs:
+                startActivity(new Intent(MainActivity.this, SimpleTabsActivity.class));
+                break;
+            case R.id.btnScrollableTabs:
+                startActivity(new Intent(MainActivity.this, ScrollableTabsActivity.class));
+                break;
+            case R.id.btnIconTextTabs:
+                startActivity(new Intent(MainActivity.this, IconTextTabsActivity.class));
+                break;
+            case R.id.btnIconTabs:
+                startActivity(new Intent(MainActivity.this, IconTabsActivity.class));
+                break;
+            case R.id.btnCustomIconTabs:
+                startActivity(new Intent(MainActivity.this, CustomViewIconTextTabsActivity.class));
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/gaurav/android_material_tabs/activity/ScrollableTabsActivity.java
@@ -0,0 +1,94 @@
+package com.gaurav.android_material_tabs.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.gaurav.android_material_tabs.R;
+import com.gaurav.android_material_tabs.fragments.EightFragment;
+import com.gaurav.android_material_tabs.fragments.FiveFragment;
+import com.gaurav.android_material_tabs.fragments.FourFragment;
+import com.gaurav.android_material_tabs.fragments.NineFragment;
+import com.gaurav.android_material_tabs.fragments.OneFragment;
+import com.gaurav.android_material_tabs.fragments.SevenFragment;
+import com.gaurav.android_material_tabs.fragments.SixFragment;
+import com.gaurav.android_material_tabs.fragments.TenFragment;
+import com.gaurav.android_material_tabs.fragments.ThreeFragment;
+import com.gaurav.android_material_tabs.fragments.TwoFragment;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ScrollableTabsActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrollable_tabs);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFrag(new OneFragment(), "ONE");
+        adapter.addFrag(new TwoFragment(), "TWO");
+        adapter.addFrag(new ThreeFragment(), "THREE");
+        adapter.addFrag(new FourFragment(), "FOUR");
+        adapter.addFrag(new FiveFragment(), "FIVE");
+        adapter.addFrag(new SixFragment(), "SIX");
+        adapter.addFrag(new SevenFragment(), "SEVEN");
+        adapter.addFrag(new EightFragment(), "EIGHT");
+        adapter.addFrag(new NineFragment(), "NINE");
+        adapter.addFrag(new TenFragment(), "TEN");
+        viewPager.setAdapter(adapter);
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFrag(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+}
